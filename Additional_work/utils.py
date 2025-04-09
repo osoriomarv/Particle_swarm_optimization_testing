452f785c44f2814c3b32bd7257caf1bbe15a9a6b
Marvin <90578878+osoriomarv@users.noreply.github.com>
2025-04-08 21:27:27 -0500

    Create utils.py
    Creating initial comments

---
+++ Additional_work/utils.py
@@ -0,0 +1,9 @@
+"""
+
+MOEA/D: A Multiobjective Evolutionary Algorithm Based on Decomposition
+
+
+Q. Zhang and H. Li, "MOEA/D: A Multiobjective Evolutionary Algorithm Based on Decomposition," in IEEE Transactions on Evolutionary Computation, vol. 11, no. 6, pp. 712-731, Dec. 2007, doi: 10.1109/TEVC.2007.892759.
+keywords: {Evolutionary computation;Pareto optimization;Computational complexity;Sorting;Genetic algorithms;Optimization methods;Testing;Scalability;Computer science;Mathematical model;Computational complexity;decomposition;evolutionary algorithm;multiobjective optimization;Pareto optimality;Computational complexity;decomposition;evolutionary algorithm;multiobjective optimization;Pareto optimality},
+
+"""
